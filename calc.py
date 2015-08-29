5887f5fdc446fc9dfab2a88fc807c234b5c0a7fb
Tiao <Tiao@whatever.com>
2015-08-29 12:28:35 -0400

    Calculator can now do very basic operations

--- calc.py
+++ calc.py
@@ -7,9 +7,7 @@
 #########
 import sys
 from operation import *
-#################################
-#Query input and find operations#
-#################################
+
 ###########
 #Variables#
 ###########
@@ -18,10 +16,16 @@ MathString = ""
 #Holds numbers to put into operations
 Arguments = list()
 #Array of nums
-IsNum = ["1","2","3","4","5","6","7","8","9","0"]
+IsNum = ["1","2","3","4","5","6","7","8","9","0","."]
+#Arry of operations
+IsOp = ["+","-","*","/","^"]
 #Temp to hold arguments
 TempArgument = list()
 #End variables
+
+#################################
+#Query input and find operations#
+#################################
 
 print "\nWelcome to a simple calculator, type help to get a full list of commands and exit to exit the program.\n"
 
@@ -32,6 +36,17 @@ while (MathString != "exit" and MathString != "exit"):
     for I in MathParts:
         if I in IsNum:
             TempArgument.append(I)
+        else:
+            TempArgument = "".join(TempArgument)
+            TempArgument = float(TempArgument)
+            Arguments.append(TempArgument)
+            TempArgument = [] 
+        if I in IsOp:
+            op = I
     TempArgument = "".join(TempArgument)
     TempArgument = float(TempArgument)
     Arguments.append(TempArgument)
+    TempArgument = [] 
+    print "______________________"
+    print operation(op,Arguments[0],Arguments[1])
+    Arguments = []
--- operation.py
+++ operation.py
@@ -2,4 +2,28 @@
 #Holds and applys math operations#
 ##################################
 
+#############################
+#List of current operations #
+#-Addition         "x+y"    #
+#-Subtraction      "x-y"    #
+#-Multiplication   "x*y"    #
+#-Devision         "x/y"    #
+#-Power            "x^y"    #
+#############################
 
+def operation(op,a,b):
+        if(op=="+"):
+		answer = a+b
+		return answer
+        if(op=="-"):
+		answer = a-b
+		return answer
+        if(op=="*"):
+		answer = a*b
+		return answer
+        if(op=="/"):
+		answer = a/b
+		return answer
+        if(op=="^"):
+		answer = a**b
+		return answer
