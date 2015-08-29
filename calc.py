a9459d6c24fcdfad0b33cdd740c44cf229f51b2a
tiao <tiao564@hotmail.com>
2015-08-29 11:14:58 -0400

    Can now read in a string of numbers and convert to integers

--- calc.py
+++ calc.py
@@ -10,5 +10,29 @@ from operation import *
 #################################
 #Query input and find operations#
 #################################
+###########
+#Variables#
+###########
+#Input function
+MathString = ""
+#Holds numbers to put into operations
+Arguments = list()
+#Array of nums
+IsNum = ["1","2","3","4","5","6","7","8","9","0"]
+#Temp to hold arguments
+TempArgument = list()
+#End variables
 
+print "\nWelcome to a simple calculator, type help to get a full list of commands and exit to exit the program.\n"
 
+#While not exit, the leave condition, accept inputs
+while (MathString != "exit" and MathString != "exit"):
+    MathString = raw_input("")
+    MathParts = list(MathString)
+    for I in MathParts:
+        if I in IsNum:
+            TempArgument.append(I)
+    TempArgument = "".join(TempArgument)
+    float(TempArgument)
+    print TempArgument
+    
